15679c2848601d731fcc007efd92e1566225dcac
Gabriel Falade <ggfalade@gmail.com>
2026-07-28 19:04:17 -0400

    start of multicycle cpu

---
+++ multicycle_cpu.srcs/sources_1/new/pc.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/28/2026 07:01:39 PM
+// Design Name: 
+// Module Name: pc
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module pc(
+
+    );
+endmodule
