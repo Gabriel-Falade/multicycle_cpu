dd5839b3198c19c21312c3deccc8f97e9b843fe7
Gabriel Falade <ggfalade@gmail.com>
2026-07-30 17:10:04 -0400

    finished pc module

---
+++ multicycle_cpu.srcs/sim_1/new/pc_tb.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/28/2026 08:45:12 PM
+// Design Name: 
+// Module Name: pc_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module pc_tb(
+
+    );
+endmodule
--- multicycle_cpu.srcs/sources_1/new/pc.v
+++ multicycle_cpu.srcs/sources_1/new/pc.v
@@ -21,6 +21,24 @@
 
 
 module pc(
-
+    input clk,    // clock 
+    input reset,        // reset
+    input PCWriteCond,  // from Control module
+    input PCWrite,      // from Control Module
+    input zero,         // from ALU
+    input[31:0] pc_next,
+    output reg[31:0] address      // since assign inside always blocked need to be 
     );
+    
+    assign pc_write_cond = (zero && PCWriteCond) || PCWrite;
+
+    always @(posedge clk) begin
+        if (reset) begin
+            address <= 32'h00000000;
+        end else if (pc_write_cond) begin
+            address <= pc_next;
+         end else begin
+            address <= address;
+        end
+       end
 endmodule
