7dacea061804e92aa2cfaed615185925761df145
Gabriel Falade <ggfalade@gmail.com>
2026-08-01 16:11:24 -0400

    instr_reg updated (did not save last time)

---
+++ multicycle_cpu.srcs/sources_1/new/alu.v
@@ -0,0 +1,32 @@
+timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08/1/2026 02:33:47 PM
+// Design Name: 
+// Module Name: alu
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module alu (
+    input srcA,     // can either be from pc or register
+    input srcB,     //
+    input alu_op,
+    output zero,
+    output reg [31:0]
+);
+
+    
+    
+endmodule
--- multicycle_cpu.srcs/sources_1/new/instr_reg.v
+++ multicycle_cpu.srcs/sources_1/new/instr_reg.v
@@ -21,6 +21,30 @@
 
 
 module instr_reg(
-
+    input clk,
+    input reset,
+    input[31:0] mem_data,     // comes from mem.v
+    input IRWrite,
+    output reg[6:0] opcode,
+    output reg[4:0] rs1,
+    output reg[4:0] rs2,
+    output reg[4:0] rd,
+    output reg[31:0] full_instr
     );
+    
+    always @(posedge clk) begin
+        if (reset) begin
+            opcode<= 7'b0000000;
+            rs1<= 5'b00000;
+            rs2<= 5'b00000;
+            rd<= 5'b00000;
+            full_instr<= 32'h0000000;
+        end else if (IRWrite) begin
+            opcode<= mem_data[6:0];
+            rs1<= mem_data[19:15];
+            rs2<= mem_data[24:20];
+            rd<= mem_data[11:7];
+            full_instr<= mem_data[31:0];
+        end
+   end
 endmodule
