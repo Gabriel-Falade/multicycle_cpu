8a99cd05aa36cf9c3301b0529ec53bf5274048c0
Gabriel Falade <ggfalade@gmail.com>
2026-08-01 22:44:12 -0400

    finished alu module

--- multicycle_cpu.srcs/sources_1/new/alu.v
+++ multicycle_cpu.srcs/sources_1/new/alu.v
@@ -20,13 +20,33 @@ timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 
 module alu (
-    input srcA,     // can either be from pc or register
-    input srcB,     //
-    input alu_op,
-    output zero,
-    output reg [31:0]
+    input [31:0]srcA,     // can either be from pc or register
+    input [31:0]srcB,     //
+    input [3:0]alu_op,
+    output reg zero,
+    output reg [31:0] alu_out
 );
 
-    
+    always @(*) begin
+        case(alu_op)
+            4'b0000: alu_out = srcA & srcB;     // bit-wise and
+            4'b0001: alu_out = srcA | srcB;     // bit-wise or
+            4'b0010: alu_out = srcA + srcB;     // addition
+            4'b0110: alu_out = srcA - srcB;     // subtration
+            4'b0011: alu_out = srcA ^ srcB;           // XOR
+            4'b0111: alu_out = srcA << srcB[4:0];     // SLL (shift left logical)
+            4'b0101: alu_out = srcA >> srcB[4:0];     // SRL (shift right logical)
+            4'b1011: alu_out = $signed(srcA) >>> srcB[4:0];             // SRA (shift right arithmetic) — needs the signed handling we discussed
+            4'b1111: alu_out = (srcA < srcB) ? 1 : 0; // SLT (set less than)
+            default: alu_out = 32'hxxxxxxxx;    // undefined/error case
+        endcase
+
+        if (alu_out == 32'd0) begin
+            zero = 1'd1;
+        end else begin
+            zero = 1'd0;
+        end
+    end
+
     
 endmodule
